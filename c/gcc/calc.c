9329e5d85e9918995e148f3de2ccf37413d79533
JunHyen Mun <impelfin@gmail.com>
2023-03-16 15:46:22 +0900

    update

---
+++ c/gcc/calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "libmy.h"
+
+void main() {
+  int x, y;
+  printf("Input two numbers : ");
+  scanf("%d %d", &x, &y);
+  printf("%d + %d = %d\n", x, y, plus(x, y));
+  printf("%d - %d = %d\n", x, y, minus(x, y));
+}
---
+++ c/gcc/minus.c
@@ -0,0 +1,3 @@
+int minus(int x, int y) {
+  return x-y;
+}
