d4e3a1249341017d79db6ffcf5e8dab43cce976e
JunHyen Mun <impelfin@gmail.com>
2023-03-20 11:26:22 +0900

    checkeod

---
+++ c/program/quiz1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "libcheckeod.h"
+
+void main() {
+  int n;
+  printf("Input Number : ");
+  scanf("%d", &n);
+  if(checkeod(n) == 0)
+    printf("%d is even number~!! \n", n);
+  else
+    printf("%d is odd number~!! \n", n);
+}
