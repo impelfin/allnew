f5cf9e34d031135fff01d7dd5fe4b88981005bd7
JunHyen Mun <impelfin@gmail.com>
2023-03-22 08:22:18 +0900

    Update useradd_opt.sh

--- shells/useradd_opt.sh
+++ shells/useradd_opt.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 #
-set -- $(getopt -q ab:c:de   "$@")
+set -- $(getopt -q u:g:c:d:s:k:m "$@")
 #
 echo
 while [ -n "$1" ]
 do
 	case "$1" in
-	-a) echo "Found the -a option" ;;
-	-b) param="$2"
-	    echo "Found the -b option, with parameter value $param"
+	-u) param="$2"
+	    echo "-u (uid) option, parameter value : $param"
+	    shift ;;
+	-g) param="$2"
+	    echo "-g (gid) option, parameter value : $param"
 	    shift ;;
 	-c) param="$2"
-	    echo "Found the -c option, with parameter value $param" 
+	    echo "-c (comment) option, parameter value : $param"
 	    shift ;;
-	--) shift
+	-d) param="$2"
+			echo "-d (home directory) option, parameter value : $param"
+			shift ;;
+	-k) param="$2"
+			echo "-k (set initial scripts directory) option, parameter value : $param"
+			shift ;;
+  -m) echo "-m (make home directory) option" ;;
+  --) shift
 	    break ;;
 	*) echo "$1 is not an option" ;;
 	esac
