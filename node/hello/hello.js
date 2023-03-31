8b5a3c6cb4104c3ed451705fd80965f4d1f3908f
impelfin <impelfin@gmail.com>
2023-03-31 16:21:36 +0900

    bottle

--- node/hello/hello.js
+++ node/hello/hello.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-    res.send("Hello Node JS~!!")
+    res.send("Hello Node JS~!")
 })
 
 app.listen(8000, function () {
