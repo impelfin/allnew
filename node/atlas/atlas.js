77371b001a83f6f30080042fd1bf6120d31e9cf9
impelfin <impelfin@gmail.com>
2023-04-04 17:52:00 +0900

    atlas

---
+++ node/atlas/atlas.js
@@ -0,0 +1,30 @@
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "testdb",
+    "database": "test",
+    "dataSource": "Cluster0",
+    "projection": {
+        "_id": 0,
+        "id": 1,
+        "name": 1
+    }
+});
+
+var config = {
+    method: 'post',
+    url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-bydgv/endpoint/data/v1/action/findOne',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': 'm7ThIAG1EPMwWTz3BJ047nFTQtoBwgVW8jlbiv9u9E9Df1vfgzI9sYUMGayOuOf7',
+    },
+    data: data
+};
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
