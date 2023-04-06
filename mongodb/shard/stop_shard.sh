7551c84453ccebd456036301f62f3ffd69b16e52
impelfin <impelfin@gmail.com>
2023-04-06 09:22:31 +0900

    shard

---
+++ mongodb/shard/stop_shard.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+config=$(ps -ef | grep 'mongodConfig')
+router=$(ps -ef | grep 'mongodRouter')
+shard1=$(ps -ef | grep 'mongodShard1')
+shard2=$(ps -ef | grep 'mongodShard2')
+
+
+second1=$(echo ${config} | cut -d " " -f2)
+second2=$(echo ${router} | cut -d " " -f2)
+second3=$(echo ${shard1} | cut -d " " -f2)
+second4=$(echo ${shard2} | cut -d " " -f2)
+
+for var in $second1 $second2 $second3 $second4
+do
+	echo $var
+	if [ -n ${var} ]; then
+		result=$(kill -9 ${var})
+		echo process is killed.
+	else
+		echo running process not found.
+	fi
+done
