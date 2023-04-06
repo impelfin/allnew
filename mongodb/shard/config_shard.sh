515f3484acf4bd544979cebf1aaddf0632ba9f0a
impelfin <impelfin@gmail.com>
2023-04-06 11:30:33 +0900

    test

--- mongodb/shard/config_shard.sh
+++ mongodb/shard/config_shard.sh
@@ -3,6 +3,12 @@
 # default mongodb daemon stop.
 systemctl stop mongod
 
+# stop shard process
+./stop_shard.sh
+
+export IP_TEMP=$(ip addr | grep enp0s3 | grep inet | cut -d " " -f6 | cut -d "/" -f1 )
+echo $IP_TEMP
+
 # remove data directory
 if [ -d data ]; then
     rm -rf ./data
@@ -15,6 +21,7 @@ touch /shard/data/logs/configsvr.log
 
 mongod --config /shard/mongodConfig.conf &
 sleep 3s
+mongo $IP_TEMP:27019 < rs.init
 
 # router Server
 touch /shard/data/logs/mongorouter.log
@@ -28,6 +35,7 @@ touch /shard/data/logs/shard1.log
 
 mongod --config /shard/mongodShard1.conf &
 sleep 2s
+mongo $IP_TEMP:27021 < rs.init
 
 # shard2 Server
 mkdir -pv /shard/data/shard2db
@@ -35,10 +43,13 @@ touch /shard/data/logs/shard2.log
 
 mongod --config /shard/mongodShard2.conf &
 sleep 2s
+mongo $IP_TEMP:27022 < rs.init
 
 # process status
 ps -ef | grep mongo
 sleep 2s
 
+mongo $IP_TEMP:27017 < rs.addShard
+
 # netstatus
-netstat -ntlp
+netstat -ntlp | grep mongo
