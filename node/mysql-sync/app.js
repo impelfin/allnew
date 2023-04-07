6eee99ebba6733bf7c1a37be32e84b986ff7d5f1
impelfin <impelfin@gmail.com>
2023-04-07 17:58:41 +0900

    mysql-sync-web

---
+++ node/mysql-sync/app.js
@@ -0,0 +1,13 @@
+var mysql = require('sync-mysql');
+const env = require('dotenv').config({ path: "../../.env" });
+
+var connection = new mysql({
+    host: process.env.host,
+    user: process.env.user,
+    port: process.env.port,
+    password: process.env.password,
+    database: process.env.database
+})
+
+let result = connection.query('select * from st_info');
+console.log(result);
