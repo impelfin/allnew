38b2d6a0bee9fc9dfb398a1357ef93ef05a5c630
impelfin <impelfin@gmail.com>
2023-04-11 10:17:39 +0900

    upup

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/hello', (req, res) => {
-    res.send('hello world~!!')
+    res.send('Hello World~!!')
 })
 
 module.exports = app;
