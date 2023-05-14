78aab90ebbe676a5abe7991026ffe3a0c746f25d
JunHyeon Mun <impelfin@gmail.com>
2023-05-14 18:29:20 +0900

    up

--- db/testdb_stinfo-stgrade_create.sql
+++ db/testdb_stinfo-stgrade_create.sql
@@ -19,9 +19,9 @@ alter table st_grade add constraint pk_stgrade primary key(ST_ID);
 explain st_info;
 explain st_grade;
 
-insert into st_info values (202201, 'LeeGiilDong', 'Game');
-insert into st_info values (202202, 'KimGiilDong', 'Computer');
-insert into st_info values (202203, 'HongGiilDong', 'Game');
+insert into st_info values (202201, 'LeeGilDong', 'Game');
+insert into st_info values (202202, 'KimGilDong', 'Computer');
+insert into st_info values (202203, 'HongGilDong', 'Game');
 select * from st_info;
 
 insert into st_grade values (202201, '80', '70');
