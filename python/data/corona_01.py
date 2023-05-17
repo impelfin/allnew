1acab63c0706d9568bd6ca3ae53f98be126b4f10
impelfin <impelfin@gmail.com>
2023-05-17 11:33:44 +0900

    up

---
+++ python/data/corona_01.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import pandas as pd 
+from datetime import datetime, timedelta
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.relpath("./")))
+secret_file = os.path.join(BASE_DIR, '../secret.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        errorMsg = "Set the {} environment variable.".format(setting)
+        return errorMsg
+
+url = 'http://apis.data.go.kr/1352000/ODMS_COVID_02/callCovid02Api'
+
+today = (datetime.today() - timedelta(1)).strftime("%Y%m%d")
+print(today)
+
+params = '?serviceKey=' + get_secret("data_apiKey")
+params += '&pageNo=1'
+params += '&numOfRows=500'
+params += '&apiType=JSON'
+params += '&status_dt=' + str(today)
+
+url += params
+print(url)
+
+response = requests.get(url)
+print(response)
+print('-' * 50)
+
+contents = response.text
+print(type(contents))
+print(contents)
+print('-' * 50)
+
+dict = json.loads(contents)
+print(type(dict))
+print(dict)
+print('-' * 50)
+
+items = dict['items']
+print(type(items))
+print(items)
+print('-' * 50)
+
+df = pd.DataFrame(items).rename(index={0:'result'}).T
+print(type(df))
+print(df)
+print('-' * 50)
+
+data = df.loc[['gPntCnt', 'hPntCnt', 'accExamCnt', 'statusDt']]
+print(type(data))
+print(data)
+print('-' * 50)
+
