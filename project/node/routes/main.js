218c9d0fc660332df42d75e40919cf5da528de77
impelfin <impelfin@gmail.com>
2023-05-24 15:42:00 +0900

    upload

--- project/node/routes/main.js
+++ project/node/routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('sync-mysql');
 const env = require('dotenv').config({ path: "../../.env" });
-const request = require('request')
+const axios = require('axios')
 
 var connection = new mysql({
     host: process.env.host,
@@ -21,6 +21,33 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/hello', (req, res) => {
     res.send('Hello World~!!')
 })
+
+app.get('/randomUUID', (req, res) => {
+    axios
+        .get('http://192.168.1.12:3000/randomUUID')
+        .then(response => {
+            console.log(`statusCode : ${response.status}`)
+            console.log(response.data)
+            res.send(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
+
+app.get('/users', (req, res) => {
+    axios
+        .get('http://192.168.1.12:5000/users')
+        .then(response => {
+            console.log(`statusCode : ${response.status}`)
+            console.log(response.data)
+            res.send(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
+
 
 // request O, query X
 app.get('/select', (req, res) => {
