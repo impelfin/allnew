7c5184e7b0cbacb6e853a85d3dbae51dff6d282f
JunHyen Mun <impelfin@gmail.com>
2023-06-20 12:40:21 +0900

    up

--- docker/buildx/server.js
+++ docker/buildx/server.js
@@ -6,7 +6,7 @@ const port='8000'
 const server = http.createServer(function (req, res) {
     res.statusCode=200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Hello from NodeJS in ${process.arch}!\n');
+    res.end(`Hello from NodeJS in ${process.arch}!\n`);
 });
 
 server.listen(port, hostname, () => {
