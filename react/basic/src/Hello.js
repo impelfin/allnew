de55e47b993b1534ef777dea29137dafa6de5117
impelfin <impelfin@gmail.com>
2023-06-30 08:23:09 +0000

    react

---
+++ react/basic/src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello({name, color }) {
+    return <div style={{ color }}>Hello~!! { name }</div>
+}
+
+Hello.defaultProps = {
+    name: 'NoName'
+}
+
+export default Hello;
