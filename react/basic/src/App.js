cfc7de9cd8fd24648010b86070c9c400c61849e8
impelfin <impelfin@gmail.com>
2023-07-06 06:27:54 +0000

    up

--- react/basic/src/App.js
+++ react/basic/src/App.js
@@ -52,10 +52,18 @@ function App() {
     nextId.current += 1;
   }
 
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(users.map(user => user.id === id ? { ...user, active: !user.active } : user));
+  }
+
   return (
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   )
 }
--- react/basic/src/UserList.js
+++ react/basic/src/UserList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
     return (
         <div>
-            <b>{user.username}</b>
+            <b style={{ corsor: 'pointer', color: user.active ? 'green' : 'black' }} onClick={() => onToggle(user.id)}>{user.username}</b>
             <span>({user.email})</span>
-        </div>
+            <button onClick={() => onRemove(user.id)}> 삭제</button>
+        </div >
     )
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} />
+                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
             ))}
         </div>
     );
