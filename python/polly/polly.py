79f2e00459ed08998486e4e117fb362842dd2349
impelfin <impelfin@gmail.com>
2023-07-25 08:29:56 +0000

    upload

--- python/polly/polly.py
+++ python/polly/polly.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 from boto3 import client
 
 polly = client("polly", region_name="ap-northeast-2")
-response = polly.synthesize_speech(
-        Text="안녕하세요. 제 이름은 서민희 앉아민희 반복하면 스퀏미니 진짜 미니 여자미니 ㅋㅋㅋㅋ",
-        OutputFormat="mp3",
-        VoiceId="Seoyeon")
+response = polly.synthesize_speech(Text="안녕하세요.  서민희 앉아민희 반복하면 스퀏미니 진짜 미니 여자미니 ㅎㅎㅎ", OutputFormat="mp3", VoiceId="Seoyeon")
 
 stream = response.get("AudioStream")
 
 with open('aws_test_tts.mp3', 'wb') as f:
     data = stream.read()
     f.write(data)
+
+cmd = "omxplayer -o both aws_test_tts.mp3"
+
+return_value = os.system(cmd)
+print('return value: ', return_value)
